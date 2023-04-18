ec56773b2e52ea843efe6b4067077cb63d9269b3
goranr <supersaiyangoran@gmail.com>
2023-04-17 22:03:05 -0600

    Debug sphere que cambia de posición dependiendo de un raycast

--- Assets/_Project/Scripts/ThirdPersonShooterController.cs
+++ Assets/_Project/Scripts/ThirdPersonShooterController.cs
@@ -14,6 +14,9 @@ public class ThirdPersonShooterController : MonoBehaviour
 	private ThirdPersonController thirdPersonController;
 	private ThirdPersonShooterInputs thirdPersonInputs;
 
+	public Transform debugAimSphere;
+	public LayerMask aimColliderMask;
+
 	private void Awake()
 	{
 		thirdPersonInputs = GetComponent<ThirdPersonShooterInputs>();
@@ -22,16 +25,35 @@ public class ThirdPersonShooterController : MonoBehaviour
 
 	private void Update()
 	{
+		Vector3 mouseWorldPosition = Vector3.zero;
+		Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
+		Ray ray = Camera.main.ScreenPointToRay(screenCenter);
+
+		if (Physics.Raycast(ray, out RaycastHit hit, 999f, aimColliderMask))
+		{
+			debugAimSphere.position = hit.point;
+			mouseWorldPosition = hit.point;
+		}
+
+
 		if (thirdPersonInputs.aim)
 		{
 			aimCamera.gameObject.SetActive(true);
 			thirdPersonController.SetSensitivity(aimSensitivity);
 
+			Vector3 worldAimTarget = mouseWorldPosition;
+			worldAimTarget.y = transform.position.y;
+			Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+
+			transform.forward = Vector3.Lerp(transform.forward, aimDirection,
+				Time.deltaTime * 20f);
 		}
 		else
 		{
 			aimCamera.gameObject.SetActive(false);
 			thirdPersonController.SetSensitivity(normalSensitivity);
 		}
+
+		
 	}
 }
